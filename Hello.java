8f99d920c21153212ef92510245a94f5f0c1197c
Pilsung Kang <pilsungk@gmail.com>
2021-04-19 10:05:03 +0900

    Update Hello.java: 2 Hi

--- Hello.java
+++ Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("World");
+        System.out.println("Hi~");
+        System.out.println("Hi2~");
     }
 }
